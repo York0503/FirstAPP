51c69760b499fc54ce95d060654aee02d48eb87e
york <york@yallvend.com>
2025-12-10 14:59:16 +0800

    feat: Implement road and car animation

--- app/src/main/java/com/example/firstapp/GameView.java
+++ app/src/main/java/com/example/firstapp/GameView.java
@@ -9,10 +9,13 @@ import android.view.View;
 
 public class GameView extends View {
 
-    private Paint carPaint;
+    private Paint carPaint; // 車子畫筆
+    private Paint roadPaint; // 車道畫筆
+    private Paint linePaint; // 車道中線畫筆(黃色的虛線)
 
-    private float carX, carY;
-    private final float carWidth = 120f, carHeight = 200f;
+    private float carX, carY; // 車子的位置
+    private final float carWidth = 120f, carHeight = 200f; // 車子本身的寬度跟高度
+    private float lineOffset = 0f; // 車道中線的偏移量
 
     private final Runnable updateRunnable = new Runnable() {
         @Override
@@ -39,19 +42,67 @@ public class GameView extends View {
     }
 
     private void init() {
+        // 車子畫筆
         carPaint = new Paint();
         carPaint.setColor(Color.RED);
+
+        // 道路畫筆
+        roadPaint = new Paint();
+        roadPaint.setColor(Color.GRAY);
+
+        // 中線畫筆
+        linePaint = new Paint();
+        linePaint.setColor(Color.YELLOW);
+        linePaint.setStrokeWidth(10f);
+
+        // 車子的初始位置
+        carX = 300f;
+        carY = 100f;
 
         post(updateRunnable);
     }
     private void update() {
+        // 車子向下移動5pixel
+        carY += 5;
+
+        // 如果車子超過底部邊界，則回到最上方
+        if(carY > getHeight()){
+            carY = -carHeight;
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawColor(Color.rgb(50, 50, 50));
+        canvas.drawColor(Color.GREEN);
+
+        float roadLeft = getWidth() * 0.2f;
+        float roadRight = getWidth() * 0.8f;
+
+        canvas.drawRect(
+                roadLeft,
+                0,
+                roadRight,
+                getHeight(),
+                roadPaint
+        );
+
+        float centerX = (roadLeft + roadRight) / 2;
+        float dashLength = 60f;
+        float gapLength = 40f;
+
+        for (float y = lineOffset; y < getHeight(); y += dashLength + gapLength){
+            canvas.drawLine(
+                    centerX,
+                    y,
+                    centerX,
+                    y + dashLength,
+                    linePaint);
+        }
+
+        float roadCenter = (roadLeft + roadRight) / 2;
+        carX = roadCenter - (carWidth / 2);
 
         canvas.drawRect(
                 carX,
